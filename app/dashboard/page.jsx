09ef2932559ce9b417b14f6606d5be57a2b9ce55
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-16 19:32:53 +0545

    Routes protected using middleware

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,14 @@
+
+
+const DashboardPage = () => {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-5">
+        Dashboard
+      </h1>
+      <div className="mb-5">Welcome to the Dashboard!</div>
+    </>
+  )
+}
+
+export default DashboardPage
