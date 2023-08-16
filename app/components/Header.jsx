bb527b676864378d13c4099e73f14ac812b610a6
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-16 19:24:01 +0545

    Clerk-App

---
+++ app/components/Header.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+
+const Header = () => {
+  return (
+    <>
+      <nav className="bg-blue-700 py-4 px-6 flex items-center justify-between mb-5">
+        <div className="flex items-center">
+        <Link href='/'>
+            <div className="text-lg uppercase font-bold text-white">
+                Clerk App
+            </div>
+        </Link>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Header
