ae51e6dff34284bcef70455cc2028bc6ff88fddb
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-17 17:45:03 +0545

    UserButton added

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,13 @@
 export default function Home() {
   return (
     <>
-      <h1>
-        Home
+      <h1 className="text-2xl font-bold.mb-5">
+        Welcome
       </h1>
+      <p className="mb-5">
+        This is a demo site for Next.js and Clerk tutorial.
+        Feel free to Sign up and Sign in.
+      </p>
     </>
   )
 }
