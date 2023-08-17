127b2f87334441eec60d1a4b0ffc5799800772e7
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-17 18:34:36 +0545

    Adding state management

---
+++ app/register/page.jsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useState } from "react"
+import { useSignUp } from "@clerk/nextjs"
+import { useRouter } from "next/navigation"
+const RegisterPage = () => {
+    const { isLoaded, signUp, setActive } = useSignUp();
+    const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [password, setPassword] = useState('');
+    const [pendingVerification, setPendingVerification] = useState(false);
+    const [code, setCode] = useState('');
+    const router = useRouter();
+  return (
+    <div>RegisterPage</div>
+  )
+}
+
+export default RegisterPage
